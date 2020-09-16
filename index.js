3db5defe09c0d7e4374759a5acc6b52178f6b8b1
avijit0685 <avijit0685@gmail.com>
2020-09-16 18:22:03 +0530

    Private Info Keep Safe

--- index.js
+++ index.js
@@ -2,22 +2,39 @@ var express = require("express");
 var app = express();
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+const { User } = require("./models/User");
+
 const connect = mongoose
-  .connect(
-    "mongodb+srv://umongo:U4JVLVFvFVpxC0jO@reactblogdb.oqymn.mongodb.net/blogDB?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-// respond with "hello world" when a GET request is made to the homepage
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Nodemon works!!");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
